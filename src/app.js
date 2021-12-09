9b3d14b25bb6521ba4423cf0437315db4077984c
Zullu Balti <zullubalti@gmail.com>
2021-12-09 15:14:06 +0500

    i think it's working

--- src/app.js
+++ src/app.js
@@ -9,17 +9,21 @@ const gui = new dat.GUI();
 
 let scene, camera, renderer, controls, gameOver;
 let speed, direction, boxes, topBox, hangBoxes;
-let world;
+let world, dirLight;
+const sizes = { width: window.innerWidth, height: window.innerHeight };
+let isPaused = false;
 
 const start = () => {
+  window.focus();
   init();
 
   scene = new THREE.Scene();
   world = new cannon.World({ gravity: new cannon.Vec3(0, -9.82, 0) });
 
   // light
-  const dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
+  dirLight = new THREE.DirectionalLight(0xffffff, 0.5);
   dirLight.position.set(10, 10, 0);
+  dirLight.shadow = true;
   dirLight.lookAt(0, 0, 0);
 
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
@@ -28,23 +32,29 @@ const start = () => {
   scene.add(dirLight);
 
   // camera
-  camera = new THREE.OrthographicCamera(-15, 15, 15, -15, 0.1, 1000);
 
+  camera = new THREE.OrthographicCamera(-20, 20, 20, -20, 0.1, 1000);
   camera.position.set(10, 10, 10);
   // camera.lookAt(0, 0, 0);
   scene.add(camera);
 
   const guiCamera = gui.addFolder("Camera");
-  guiCamera.add(camera.position, "x", -300, 300, 1);
-  guiCamera.add(camera.position, "y", -300, 300, 1);
-  guiCamera.add(camera.position, "z", -300, 300, 1);
+  guiCamera.add(camera.position, "x", -100, 100, 1);
+  guiCamera.add(camera.position, "y", -100, 100, 1);
+  guiCamera.add(camera.position, "z", -100, 100, 1);
 
-  guiCamera.add(camera.rotation, "x", -Math.PI, Math.PI, 0.1);
-  guiCamera.add(camera.rotation, "y", -Math.PI, Math.PI, 0.1);
-  guiCamera.add(camera.rotation, "z", -Math.PI, Math.PI, 0.1);
+  const guiCameraRot = gui.addFolder("Camera Rot");
+
+  guiCameraRot.add(camera.rotation, "x", -Math.PI, Math.PI, 0.1);
+  guiCameraRot.add(camera.rotation, "y", -Math.PI, Math.PI, 0.1);
+  guiCameraRot.add(camera.rotation, "z", -Math.PI, Math.PI, 0.1);
 
   const cameraHelper = new THREE.CameraHelper(camera);
   scene.add(cameraHelper);
+
+  // axes helper
+  const axesHelper = new THREE.AxesHelper();
+  scene.add(axesHelper);
 
   // basic object
   addBox();
@@ -54,7 +64,7 @@ const start = () => {
   const canvas = document.querySelector("canvas#game_container");
   renderer = new THREE.WebGLRenderer({ antialias: true, canvas });
   renderer.render(scene, camera);
-  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(2, window.devicePixelRatio));
 
   controls = new OrbitControls(camera, canvas);
@@ -75,39 +85,48 @@ function init() {
   toggleGameOverCon();
 }
 
+const clock = new THREE.Clock();
+
 const animate = () => {
   renderer.render(scene, camera);
   controls.update();
+  const timePassed = clock.getDelta();
 
-  world.step(1 / 60);
-  if (!gameOver && topBox) {
-    topBox.threejs.position[direction] += speed;
-    topBox.cannonjs.position[direction] += speed;
+  if (!isPaused) {
+    world.step(1 / 60);
+    if (!gameOver && topBox) {
+      topBox.threejs.position[direction] += speed;
+      topBox.cannonjs.position[direction] += speed;
 
-    for (let i = 0; i < hangBoxes.length; i++) {
-      const box = hangBoxes[i];
+      for (let i = 0; i < hangBoxes.length; i++) {
+        const box = hangBoxes[i];
 
-      box.threejs.position.y = box.cannonjs.position.y;
-      if (box.cannonjs.position.y < -100) {
-        world.removeBody(box.cannonjs);
-        scene.remove(box.threejs); // this could fuck it up for some strange reasons about indexes
-        hangBoxes.splice(i, 1);
-        i--;
+        box.threejs.position.y = box.cannonjs.position.y;
+        if (box.cannonjs.position.y < -100) {
+          world.removeBody(box.cannonjs);
+          scene.remove(box.threejs); // this could fuck it up for some strange reasons about indexes
+          hangBoxes.splice(i, 1);
+          i--;
+        }
+      }
+
+      isGameOver();
+    }
+
+    if (gameOver && topBox) {
+      const { y } = topBox.cannonjs.position;
+      topBox.threejs.position.y = y;
+
+      if (y < -100) {
+        scene.remove(topBox.threejs);
+        world.removeBody(topBox.cannonjs);
+        topBox = undefined;
       }
     }
 
-    isGameOver();
-  }
-
-  if (gameOver && topBox) {
-    const { y } = topBox.cannonjs.position;
-    console.log(y);
-    topBox.threejs.position.y = y;
-
-    if (y < -100) {
-      scene.remove(topBox.threejs);
-      world.removeBody(topBox.cannonjs);
-      topBox = undefined;
+    if (camera.position.y < (boxes.length - 2) * 3 + 10) {
+      camera.position.y += 1;
+      dirLight.position.y += 1;
     }
   }
 
@@ -127,8 +146,10 @@ const addBox = () => {
 
   const geometry = new THREE.BoxBufferGeometry(width, height, depth);
   const material = new THREE.MeshLambertMaterial({
-    color: 0xff00ff,
+    color: new THREE.Color(`hsl(${30 + (boxes.length % 10) * 2} , 100%, 50%)`),
     // wireframe: true,
+    shadowSide: true,
+    clipShadows: true,
   });
 
   const mesh = new THREE.Mesh(geometry, material);
@@ -147,7 +168,7 @@ const addBox = () => {
     body.position.set(0, 0, 0);
   } else {
     topBox = boxes[boxes.length - 1];
-    let { y, x, z } = topBox.threejs.position;
+    let { x, y, z } = topBox.threejs.position;
     x = direction === "z" ? -30 : x;
     z = direction === "x" ? -30 : z;
 
@@ -164,38 +185,15 @@ const addBox = () => {
   updateScore();
 };
 
-const addHangBox = (width, height, depth, x, y, z) => {
-  const geometry = new THREE.BoxBufferGeometry(width, height, depth);
-  const material = new THREE.MeshLambertMaterial({
-    color: 0xff00ff,
-    // wireframe: true,
-  });
-  const mesh = new THREE.Mesh(geometry, material);
-
-  const shape = new cannon.Box(
-    new cannon.Vec3(width / 2, height / 2, depth / 2)
-  );
-  const body = new cannon.Body({ mass: 1, shape });
-  mesh.position.set(x, y, z);
-  body.position.set(x, y, z);
-
-  scene.add(mesh);
-  world.addBody(body);
-  const hangBox = { threejs: mesh, cannonjs: body };
-  hangBoxes.push(hangBox);
-};
-
 const removeAllBoxes = () => {
   // remove all boxes
   for (let i = scene?.children.length - 1; i >= 0; i--) {
-    console.log(scene.children[i].type);
     if (scene.children[i].type === "Mesh") scene.remove(scene.children[i]);
   }
 };
 
 const isGameOver = () => {
   if (topBox.threejs.position[direction] >= 30 || gameOver === true) {
-    console.log("game over");
     gameOver = true;
     toggleGameOverCon();
   }
@@ -220,6 +218,8 @@ document.addEventListener("click", () => {
 document.addEventListener("keypress", (e) => {
   if (e.code === "Space") {
     stopBox();
+  } else if (e.code === "KeyP") {
+    isPaused = !isPaused;
   } else if (e.code === "KeyR") {
     init();
     addBox();
@@ -227,21 +227,98 @@ document.addEventListener("keypress", (e) => {
   }
 });
 
+const findDistanceBtwTopNBtm = () => {
+  const bottomBox = boxes[boxes.length - 2];
+  const tpd = topBox.threejs.position[direction];
+  const bpd = bottomBox.threejs.position[direction];
+  return tpd - bpd;
+};
+
+const findScaleDown = () => {
+  const distance = findDistanceBtwTopNBtm();
+  const size = getCurrentSize();
+  return (size - Math.abs(distance)) / size;
+};
+
+const getCurrentSize = () => {
+  return direction === "x" ? topBox.width : topBox.depth;
+};
+
+const setRemainingPart = () => {
+  const scale = findScaleDown();
+  const distance = findDistanceBtwTopNBtm();
+  const updatedSize = getCurrentSize() - Math.abs(distance); // also size - diff
+  const updatedPosition = (getCurrentSize() - updatedSize) / 2; // topBox width/2  - remaining width
+
+  topBox.threejs.scale[direction] = scale;
+
+  if (distance > 0) {
+    topBox.threejs.position[direction] -= updatedPosition;
+  } else {
+    topBox.threejs.position[direction] += updatedPosition;
+  }
+
+  world.removeBody(topBox.cannonjs);
+  let { width, depth, height } = topBox;
+  const { x, y, z } = topBox.threejs.position;
+
+  if (direction === "x") {
+    width *= scale;
+  } else depth *= scale;
+
+  const shape = new cannon.Box(
+    new cannon.Vec3(width / 2, height / 2, depth / 2)
+  );
+  const body = new cannon.Body({ mass: 0, shape });
+  body.position.set(x, y, z);
+  world.addBody(body);
+  topBox.cannonjs = body;
+};
+
+const findUpdatedSize = () => {
+  let { width, height, depth } = topBox;
+  const distance = findDistanceBtwTopNBtm();
+  let { x, y, z } = topBox.threejs.position;
+
+  if (direction === "x") {
+    x += (width / 2) * (distance > 0 ? 1 : -1);
+    width = Math.abs(distance) * 2;
+  } else {
+    z += (depth / 2) * (distance > 0 ? 1 : -1);
+    depth = Math.abs(distance) * 2;
+  }
+
+  return { width, height, depth, x, y, z };
+};
+
+const setHangingPart = () => {
+  let { width, height, depth, x, y, z } = findUpdatedSize();
+  const color = `hsl(${30 + (boxes.length % 10) * 2}, 100%, 50%)`;
+
+  const geometry = new THREE.BoxBufferGeometry(width, height, depth);
+  const material = new THREE.MeshLambertMaterial({
+    color: new THREE.Color(color),
+  });
+  const mesh = new THREE.Mesh(geometry, material);
+
+  const shape = new cannon.Box(
+    new cannon.Vec3(width / 2, height / 2, depth / 2)
+  );
+
+  const body = new cannon.Body({ mass: 1, shape });
+
+  mesh.position.set(x, y, z);
+  body.position.set(x, y, z);
+
+  scene.add(mesh);
+  world.addBody(body);
+  const hangBox = { threejs: mesh, cannonjs: body };
+  hangBoxes.push(hangBox);
+};
+
 const stopBox = () => {
   if (boxes.length > 1) {
-    const bottomBox = boxes[boxes.length - 2];
-
-    const topP = topBox.threejs.position[direction] - 0;
-    // bottomBox.threejs.position[direction];
-
-    const bottomP = bottomBox.threejs.position[direction];
-    const bottomW = bottomBox.width;
-    const bottomD = bottomBox.depth;
-    let isOutside = false;
-    const bSize = direction === "x" ? bottomW : bottomD;
-    const topPosi = topBox.threejs.position[direction];
-
-    isOutside = topPosi <= bottomP - bSize || topPosi >= bottomP + bSize;
+    const isOutside = Math.abs(findDistanceBtwTopNBtm()) >= getCurrentSize();
 
     if (isOutside) {
       topBox.cannonjs.mass = 1;
@@ -251,62 +328,16 @@ const stopBox = () => {
       return;
     }
 
-    let hX, hY, hZ;
-    let hWidth = topBox.width;
-    let hDepth = topBox.depth;
-    let size = direction === "x" ? topBox.width : topBox.depth;
+    const remSize = findScaleDown() * getCurrentSize();
 
-    const scaleP = (size - Math.abs(topP)) / size;
+    setRemainingPart();
+    setHangingPart();
 
-    if (direction === "x") hWidth = Math.abs(topP);
-    else if (direction === "z") hDepth = Math.abs(topP);
-
-    hX = topBox.threejs.position.x;
-    hZ = topBox.threejs.position.z;
-    hY = topBox.threejs.position.y;
-
-    topBox.threejs.scale[direction] = scaleP;
-
-    if (topP < 0) {
-      topBox.threejs.position[direction] = -size / 2 + scaleP * (size / 2);
-      if (direction === "z") hZ -= (scaleP * size) / 2;
-      else hX -= (scaleP * size) / 2;
-    } else {
-      topBox.threejs.position[direction] = size / 2 - (scaleP * size) / 2;
-
-      if (direction === "z") hZ += (scaleP * size) / 2;
-      else hX += (scaleP * size) / 2;
-    }
-
-    addHangBox(hWidth, 3, hDepth, hX, hY, hZ);
-    slicePhysicsBox(scaleP * size);
-
-    if (direction === "x") topBox.width -= Math.abs(topP);
-    else topBox.depth -= Math.abs(topP);
+    if (direction === "x") topBox.width = remSize;
+    else topBox.depth = remSize;
   }
 
   addBox();
-};
-
-const slicePhysicsBox = (scale) => {
-  const { x, y, z } = topBox.threejs.position;
-  console.log(topBox.cannonjs);
-  let { x: width, y: height, z: depth } = topBox.cannonjs.shapes[0].halfExtents;
-
-  if (direction === "x") width = scale / 2;
-  else if (direction === "z") depth = scale / 2;
-
-  const shape = new cannon.Box(new cannon.Vec3(width, height, depth));
-  const body = new cannon.Body({
-    mass: 0,
-    shape,
-    type: cannon.Body.DYNAMIC,
-  });
-  body.position.set(x, y, z);
-
-  world.removeBody(topBox.cannonjs);
-  world.addBody(body);
-  topBox.cannonjs = body;
 };
 
 const updateScore = () => {
